97e66a71ff87be79406281efa76ba1b0f44bbdc4
cyril-maandal <cyril.maandal@publicissapient.com>
2022-04-05 09:00:02 -0400

    Initial commit

---
+++ src/main/java/com/example/GetJabbed/GetJabbedApplication.java
@@ -0,0 +1,13 @@
+package com.example.GetJabbed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetJabbedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetJabbedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GetJabbed/GetJabbedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GetJabbed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetJabbedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
